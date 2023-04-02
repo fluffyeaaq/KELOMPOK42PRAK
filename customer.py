4226d00ae26b81f52180bd09b4bdd2008ce33882
NOFIA NUR KHASANAH <129140023+fluffyeaaq@users.noreply.github.com>
2023-04-02 15:33:28 +0700

    Update customer.py

--- customer.py
+++ customer.py
@@ -2,16 +2,9 @@ from atm_card import ATMCard
 
 class Customer: 
     def __init__(self,id,CustPin=422022,CustBalance=1000000):
-        self.id = id 
-        self.pin = CustPin
-        self.balance = CustBalance 
     def checkId(self): 
-        return self.id 
     def checkPin(self):
-        return self.pin
     def checkBalance(self):
-        return self.balance 
     def withdrawBalance(self, nominal):
-        self.balance -= nominal
     def depositBalance(self, nominal):
-        self.balance += nominal
+        
