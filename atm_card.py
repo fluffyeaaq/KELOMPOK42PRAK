35d4a8eff05a193f83aa31e02cbe56f869214e06
Ilfaaa <129637495+Ilfaaa@users.noreply.github.com>
2023-04-02 10:05:27 +0700

    Update atm_card.py

--- atm_card.py
+++ atm_card.py
@@ -2,7 +2,7 @@ class ATMCard
     def __init__(self, defaultPin, defaultBalance): 
         self.defaultPin = defaultPin
         self.defaultBalance = defaultBalance
-    def CekPinAwal(self)
+    def CekPinAwal(self):
         return self.defaultPin 
-    def CekSaldoAwal(self) 
+    def CekSaldoAwal(self): 
         return self.defaultBalance
