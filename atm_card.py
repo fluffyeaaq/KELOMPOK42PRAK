ba30007eaf904ab1432b37511ff5c5a92e9e2b31
NOFIA NUR KHASANAH <129140023+fluffyeaaq@users.noreply.github.com>
2023-04-02 10:00:54 +0700

    Update atm_card.py

--- atm_card.py
+++ atm_card.py
@@ -1,8 +1,8 @@
-class ATMCard: 
+class ATMCard
     def __init__(self, defaultPin, defaultBalance): 
         self.defaultPin = defaultPin
         self.defaultBalance = defaultBalance
-    def CekPinAwal(self): 
+    def CekPinAwal(self)
         return self.defaultPin 
-    def CekSaldoAwal(self): 
+    def CekSaldoAwal(self) 
         return self.defaultBalance
